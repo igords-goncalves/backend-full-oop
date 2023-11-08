576ab4772bea6292ff3e42c62bc4727ad82b9abf
Igor Gonçalves <igordossantos38@gmail.com>
2023-11-07 23:20:35 -0300

    feat: implement inheritance in models

--- src/controllers/usuarioController.ts
+++ src/controllers/usuarioController.ts
@@ -1,86 +1,152 @@
 import { Request, Response } from "express";
-import Usuario from "../models/Pessoa/Usuario.ts";
 import Aula from "../models/Aula/Aula.ts";
+import Aluno from "../models/Entidades/Aluno.ts";
+import Professor from "../models/Entidades/Professor.ts";
+import Funcionario from "../models/Entidades/Funcionario.ts";
 
-type usuario = {
+type usuarios = {
   nome: string;
   idade: number;
   email: string;
-  aluno: Usuario;
-  professor: Usuario;
+  matricula: number;
+  curso: string;
+  salario: number;
+  especialidade: string;
+  setor: string;
+  trabalhando: boolean;
 };
 
 export default class usuarioController {
-  static criarUsuario(req: Request, res: Response) {
-    const { nome, idade, email }: usuario = req.body;
+  static criarAluno(req: Request, res: Response) {
+    const { nome, idade, email, matricula, curso }: usuarios = req.body;
 
-    const newUser = new Usuario(nome, idade, email);
-
-    // A lógica que faz as validações está no model
-    // só estou utilizando o método
-    if (newUser.validarEmail()) {
+    const aluno = new Aluno(nome, idade, email, matricula, curso);
+    if (aluno.validarEmail()) {
       res.status(400).json({
         error: true,
-        message: newUser.validarEmail(),
+        message: aluno.validarEmail(),
       });
-      res.end();
-    } else if (newUser.validarIdade()) {
-      res.status(400).json({
-        error: true,
-        message: newUser.validarIdade(),
-      });
-      res.end();
-    } else {
-      res.status(201).json({
-        error: false,
-        message: "Usuário adicionado com sucesso.",
-        usuario: newUser,
-        bio: newUser.apresentar(),
-        boasVindas: newUser.enviarEmailBoasVindas(),
-      });
+      return res.end();
     }
+
+    return res.status(201).json({
+      error: false,
+      message: "Aluno adicionado com sucesso.",
+      aluno: aluno,
+      bio: aluno.apresentar(),
+      boasVindas: aluno.enviarEmailBoasVindas(),
+    });
+  }
+
+  static criarProfessor(req: Request, res: Response) {
+    const { nome, idade, email, salario, especialidade }: usuarios = req.body;
+    const professor = new Professor(nome, idade, email, salario, especialidade);
+
+    if (professor.validarEmail()) {
+      res.status(400).json({
+        error: true,
+        message: professor.validarEmail(),
+      });
+      return res.end();
+    }
+
+    if (professor.validarIdade()) {
+      res.status(400).json({
+        error: true,
+        message: professor.validarIdade(),
+      });
+      return res.end();
+    }
+
+    return res.status(201).json({
+      error: false,
+      message: "Professor adicionado com sucesso.",
+      professor: professor,
+      bio: professor.apresentar(),
+      boasVindas: professor.enviarEmailBoasVindas(),
+    });
+  }
+
+  static criarFuncionario(req: Request, res: Response) {
+    const { nome, idade, email, setor, trabalhando }: usuarios = req.body;
+    const funcionario = new Funcionario(nome, idade, email, setor, trabalhando);
+
+    if (funcionario.validarIdade()) {
+      res.status(400).json({
+        error: true,
+        message: funcionario.validarIdade(),
+      });
+      return res.end();
+    }
+
+    return res.status(201).json({
+      error: false,
+      message: "Funcionário adicionado com sucesso.",
+      funcionario: funcionario,
+      bio: funcionario.apresentar(),
+      boasVindas: funcionario.enviarEmailBoasVindas(),
+    });
   }
 
   static getUsuarios(req: Request, res: Response) {
-    // TODO: Como definir esse usuário mock abaixo diretamente pelo contrutor?
     const user = {
       aluno: {
-        nome: "José",
-        idade: 55,
-        email: "ze@email.com",
+        nome: "Naldo",
+        idade: 23,
+        email: "naldo@email.com",
+        matricula: 123456,
+        curso: "Engenharia",
       },
       professor: {
         nome: "José",
         idade: 55,
         email: "ze@email.com",
+        salario: 5000,
+        especialidade: "Matemática",
+      },
+      funcionario: {
+        nome: "Ana",
+        idade: 32,
+        email: "ana@email.com",
+        setor: "Financeiro",
+        trabalhando: true,
       },
     };
 
-    // Professor cria uma aula
-    const newProfessor = new Usuario(
+    const professor = new Professor(
       user.professor.nome,
       user.professor.idade,
       user.professor.email,
+      user.professor.salario,
+      user.professor.especialidade,
     );
 
-    // Aluno assiste uma aula
-    const newAluno = new Usuario(
+    const aluno = new Aluno(
       user.aluno.nome,
       user.aluno.idade,
       user.aluno.email,
+      user.aluno.matricula,
+      user.aluno.curso,
     );
 
-    // Uma aula está acontecendo, através da classe relacionada!
-    const aulaDeCiencias = new Aula(newAluno, newProfessor);
+    const funcionario = new Funcionario(
+      user.funcionario.nome,
+      user.funcionario.idade,
+      user.funcionario.email,
+      user.funcionario.setor,
+      user.funcionario.trabalhando,
+    );
+
+    const aulaDeCiencias = new Aula(aluno, professor);
 
     return res.status(201).json({
       error: false,
-      professor: newProfessor,
-      bio: newProfessor.apresentar(),
-      boasVindas: newProfessor.enviarEmailBoasVindas(),
-      aluno: newAluno,
-      agendaLivre: aulaDeCiencias.marcarAula(),
+      professor: professor,
+      aluno: aluno,
+      funcionario: funcionario,
+      aula: aulaDeCiencias.marcarAula(),
       aulaAprovada: aulaDeCiencias.aprovarAula(),
+      aumentoProfessor: professor.receberAumento(5.5),
     });
   }
 }
