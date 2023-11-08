15db1e226e20f9e755ee7abda249602b771a8815
Igor Gonçalves <igordossantos38@gmail.com>
2023-11-07 23:22:24 -0300

    feat: change users type

--- src/models/Aula/Aula.ts
+++ src/models/Aula/Aula.ts
@@ -1,31 +1,19 @@
-import Usuario from "../Pessoa/Usuario";
 import IAula from "../../interfaces/IAula";
+import Aluno from "../Entidades/Aluno";
+import Professor from "../Entidades/Professor";
 
 export default class Aula implements IAula {
   constructor(
-    private aluno: Usuario,
-    private professor: Usuario,
+    private aluno: Aluno,
+    private professor: Professor,
   ) {}
 
   public marcarAula(): boolean {
-    if (!this.Aluno || !this.Professor) return false;
+    if (!this.aluno || !this.professor) return false;
     return true;
   }
   public aprovarAula(): boolean {
     if (!this.professor) return false;
     return true;
-  }
-
-  public get Professor(): Usuario {
-    return this.professor;
-  }
-  public set Professor(value: Usuario) {
-    this.professor = value;
-  }
-  public get Aluno(): Usuario {
-    return this.aluno;
-  }
-  public set Aluno(value: Usuario) {
-    this.aluno = value;
   }
 }
