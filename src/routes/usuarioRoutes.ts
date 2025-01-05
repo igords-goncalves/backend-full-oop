e11fec5dcea30a462907a6dc936bf2d86c387ebe
Igor Gonçalves <igordossantos38@gmail.com>
2025-01-05 17:51:28 -0300

    feat: update usuarioRoutes to use specific controllers for Aluno, Professor, and Funcionario creation

--- src/routes/usuarioRoutes.ts
+++ src/routes/usuarioRoutes.ts
@@ -1,11 +1,14 @@
 import express from "express";
 import usuarioController from "../controllers/usuarioController.ts";
+import ProfessorController from "../controllers/professor.controller.ts";
+import AlunoController from "../controllers/aluno.controller.ts";
+import FuncionarioController from "../controllers/funcionario.controller.ts";
 
 const usuarioRoutes = express.Router();
 
 usuarioRoutes.get("/usuarios", usuarioController.getUsuarios);
-usuarioRoutes.post("/criar/aluno", usuarioController.criarAluno);
-usuarioRoutes.post("/criar/prof", usuarioController.criarProfessor);
-usuarioRoutes.post("/criar/func", usuarioController.criarFuncionario);
+usuarioRoutes.post("/criar/aluno", AlunoController.criarAluno);
+usuarioRoutes.post("/criar/prof", ProfessorController.criarProfessor);
+usuarioRoutes.post("/criar/func", FuncionarioController.criarFuncionario);
 
 export default usuarioRoutes;
